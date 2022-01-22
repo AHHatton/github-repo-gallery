add3b5473860f6b44bd83b6af7bbe7a98f1721df
AHHatton <amanda.h.hatton@gmail.com>
2022-01-21 17:43:08 -0900

    Added async functions to pull API data from github

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
 const overview = document.querySelector(".overview"); //element with overview class
 const username = "AHHatton"; //my github username
+const repoList = document.querySelector(".repo-list"); //selects ul to display repos
 
 const getInfo = async function() { //select user info from github API
 	const request = await fetch (`https://api.github.com/users/${username}`);
 	const data = await request.json();
-	console.log(data);
+	//console.log(data);
 	userData(data);
 };
 
-const userData = function(data){ //Pull info from getInfo function and display in new div
+getInfo();
+
+const userData = function(data){ // display info from GetInfo function in new div
 	const userInfoDiv = document.createElement("div");
-	//div.classlist.add("user-info");
+	userInfoDiv.classList.add("user-info"); //hmm
 	userInfoDiv.innerHTML = `
 	<figure>
       <img alt="user avatar" src=${data.avatar_url} />
@@ -22,7 +25,21 @@ const userData = function(data){ //Pull info from getInfo function and display i
       <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
     </div>`;
   overview.append(userInfoDiv);
+  getRepos();
 };
 
+const getRepos = async function() { //get repo info from github
+	const requestRepos = await fetch (`https://api.github.com/users/${username}/repos?per_page=100&sort=updated`);
+	const dataRepos = await requestRepos.json();
+	console.log(dataRepos);
+	displayRepo(dataRepos);
+};
 
-getInfo();
+const displayRepo = function(repos) { //function to display repo information
+  for (const repo of repos) {
+  	const repoListItem = document.createElement("li");
+    repoListItem.classList.add("repo"); 
+    repoListItem.innerHTML = `<h3>${repo.name}</h3>`;
+  	repoList.append(repoListItem);
+  }
+};
